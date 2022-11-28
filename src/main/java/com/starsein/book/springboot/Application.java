d3dbd8b0466ec60f98e8ec47860e87735ac7040d
hwang-yuseong <dbtjd1928@gmail.com>
2022-11-29 02:09:18 +0900

    Chapter 03 반영

--- src/main/java/com/starsein/book/springboot/Application.java
+++ src/main/java/com/starsein/book/springboot/Application.java
@@ -2,7 +2,9 @@ package com.starsein.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication  // 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성 모두 자동 설정
                         // 여기서부터 설정을 읽어가므로 이 클래스는 항상 프로젝트의 최상단에 위치
 public class Application {  // 이 프로젝트의 메인 클래스
